6e49f827e5e38bcb787f1e9219403493f7dd3df2
nick.hughes <nick.hughes@touchsurgery.com>
2019-10-17 17:24:07 +0100

    Formatting defaults and flake 8 clean

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "core",
+    "blog",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -94,8 +97,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
+    },
 ]
 
 
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, "index.html", {"title": "Index Page"})
+
+
+def about_page(request):
+    return render(request, "about.html", {"title": "About Page"})
+
+
+def contact_page(request):
+    return render(request, "contact.html", {"title": "Contact Page"})

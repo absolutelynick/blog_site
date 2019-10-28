64e753a6081e9364ef55897946768f3b0179e2a1
nick.hughes <nick.hughes@touchsurgery.com>
2019-10-28 17:07:39 +0000

    First working setup with a blog model to test the postgres db

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("/site/admin/", admin.site.urls),
+    path("site/admin/", admin.site.urls),
     path("", include("core.urls")),
 ]
